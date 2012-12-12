1159c33c4a749769008886760620e16dcca3aac3
Edwin Schreuder <enjschreuder@gmail.com>
2012-12-12 11:02:52 +0100

    Removed gconf from lcdproc_config_dialog.py

--- lcdproc-plugin/lcdproc_config_dialog.py
+++ lcdproc-plugin/lcdproc_config_dialog.py
@@ -19,9 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-import gobject
-#import gtk
-import gconf, gnome
+from gi.repository import GObject, Peas, RB
 
 #FIXME This must probably be redone since API changes in Rhythmbox
 
